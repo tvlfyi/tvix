801bbb16f7a1f8f319589f841a5a90262916a144
Vincent Ambo <mail@tazj.in>
2021-03-27 00:49:52 +0200

    feat(tvix/proto): Add evaluator communication proto
    This adds the proto messages and service definition used for
    communicating with our evaluator.
    
    Please refer to the `Tvix - Component interaction` document for a
    detailed description of the interactions that this service is used
    for.
    
    Change-Id: If44cdbff66c6cfe88c22a748fe29c0bbd8510b0d
    Co-Authored-By: Florian Klink <flokli@flokli.de>
    Co-Authored-By: Adam Höse <adisbladis@gmail.com>
    Reviewed-on: https://cl.tvl.fyi/c/depot/+/2685
    Tested-by: BuildkiteCI
    Reviewed-by: flokli <flokli@flokli.de>
    Reviewed-by: grfn <grfn@gws.fyi>

---
+++ proto/default.nix
@@ -0,0 +1,9 @@
+# Build protocol buffer definitions to ensure that protos are valid in
+# CI. Note that the output of this build target is not actually used
+# anywhere, it just functions as a CI check for now.
+{ pkgs, ... }:
+
+pkgs.runCommandNoCC "tvix-cc-proto" {} ''
+  mkdir $out
+  ${pkgs.protobuf}/bin/protoc -I ${./.} evaluator.proto --cpp_out=$out
+''
