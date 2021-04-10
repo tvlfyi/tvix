5d916d7377120406b5c37f1dc54a9cefbc40aa79
Vincent Ambo <mail@tazj.in>
2021-04-10 18:05:16 +0200

    refactor: Move nixpkgs attribute to `third_party.nixpkgs`
    Please read b/108 to make sense of this.
    
    This gets rid of the explicit list of exposed packages from nixpkgs,
    and instead makes the entire package set available at
    `third_party.nixpkgs`.
    
    To accommodate this, a LOT of things have to be very slightly shuffled
    around. Some of this was done in already submitted CLs, but this
    change is unfortunately still quite noisy.
    
    Pay extra attention to:
    
    * overlay-like functionality that was partially moved to actual
      overlays (partially as in, the minimum required to get a green
      build)
    
    * modified uses of the package set path, esp. in NixOS systems
    
    Special notes:
    
    * xanthous has been disabled in CI because of issues with the Haskell
      overlay
    * //third_party/nix has been disabled because of other unclear
      dependency issues
    
    Both of these will be tackled in a followup CL.
    
    Change-Id: I2f9c60a4d275fdb5209264be0addfd7e06c53118
    Reviewed-on: https://cl.tvl.fyi/c/depot/+/2910
    Reviewed-by: glittershark <grfn@gws.fyi>
    Reviewed-by: sterni <sternenseemann@systemli.org>
    Tested-by: BuildkiteCI

---
+++ nixpkgs/default.nix
@@ -0,0 +1,56 @@
+# This file imports the pinned nixpkgs sets and applies relevant
+# modifications, such as our overlays.
+#
+# Note that the attribute exposed by this (third_party.nixpkgs) is
+# "special" in that the fixpoint used as readTree's config parameter
+# in //default.nix passes this attribute as the `pkgs` argument to all
+# readTree derivations.
+
+{ depot, ... }:
+
+let
+  # This provides the sources of nixpkgs. We track both
+  # nixos-unstable, and the current stable channel of the latest NixOS
+  # release.
+
+  # Tracking nixos-unstable as of 2021-03-25.
+  unstableHashes = {
+    commit = "60dd94fb7e01a8288f6638eee71d7cb354c49327";
+    sha256 = "0skdwk9bdld295kzrymirs8xrzycqmhsclaz8s18jhcz75hb8sk3";
+  };
+
+  # Tracking nixos-20.09 as of 2021-03-25.
+  stableHashes = {
+    commit = "223d0d733a66b46504ea6b4c15f88b7cc4db58fb";
+    sha256 = "073327ris0frqa3kpid3nsjr9w8yx2z83xpsc24w898mrs9r7d5v";
+  };
+
+  nixpkgsSrc = fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/${unstableHashes.commit}.tar.gz";
+    sha256 = unstableHashes.sha256;
+  };
+  stableNixpkgsSrc = fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/${stableHashes.commit}.tar.gz";
+    sha256 = stableHashes.sha256;
+  };
+
+  # Stable package set is imported, but not exposed, to overlay
+  # required packages into the unstable set.
+  stableNixpkgs = import stableNixpkgsSrc {};
+
+  # Overlay for packages that should come from the stable channel
+  # instead (e.g. because something is broken in unstable).
+  stableOverlay = self: super: {
+    inherit (stableNixpkgs)
+      awscli # TODO(grfn): Move back to unstable once it is fixed
+      ;
+  };
+in import nixpkgsSrc {
+  config.allowUnfree = true;
+  config.allowBroken = true;
+  overlays = [
+    stableOverlay
+    depot.third_party.overlays.tvl
+    depot.third_party.overlays.haskell
+  ];
+}
