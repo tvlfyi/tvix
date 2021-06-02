66d08c4f8489af1b680b99e6af0ad3576f510c40
Vincent Ambo <mail@tazj.in>
2021-06-02 22:32:41 +0200

    chore(3p/nixpkgs): Bump channels to 2021-06-05
    Includes the following depot changes & fixes:
    
    * stable moves to NixOS 21.05
    * stable isn't used anymore (but we'll keep the mechanism)
    * haskell overlay's `random` override is removed (YAY!)
    * grfn/iso: Switch to regular kernel rather than
      latest kernel, as latest kernel is currently marked as broken due to zfs
    * grfn/home: Use julia_16-bin temporarily
      julia 1.5 (current julia-stable, source built release in nixpkgs)
      doesn't pass its own test suite. Julia 1.6 doesn't have a source built
      package in nixpkgs yet, so julia_16-bin appears to be the only working
      julia derivation currently.
    * tazjin/tverskoy: Use zfs unstable, as stable zfs doesn't work with the
      latest kernel
    
    Co-Authored-By: Griffin Smith <grfn@gws.fyi>
    Co-Authored-By: sterni <sternenseemann@systemli.org>
    Change-Id: I6f2e3d9f75077e4755de6bde9104d44b584cbe4c
    Reviewed-on: https://cl.tvl.fyi/c/depot/+/3174
    Tested-by: BuildkiteCI
    Reviewed-by: sterni <sternenseemann@systemli.org>
    Reviewed-by: tazjin <mail@tazj.in>
    Reviewed-by: grfn <grfn@gws.fyi>

--- nixpkgs/default.nix
+++ nixpkgs/default.nix
@@ -13,16 +13,16 @@ let
   # nixos-unstable, and the current stable channel of the latest NixOS
   # release.
 
-  # Tracking nixos-unstable as of 2021-05-25.
+  # Tracking nixpkgs-unstable as of 2021-06-05.
   unstableHashes = {
-    commit = "900115a4f7fdd9189e7803ca781a65be663f2c89";
-    sha256 = "11551nawxjbgya8sq1p6ghkbws9qz9fbfq3wqawm3zh8ayr4l13j";
+    commit = "2a1c29ef4bacac06f9b677931027bf053952618c";
+    sha256 = "1gxkvp4bf222v23kpb7di8iay5gl2qyv5qwgmb10nzdr87avbcax";
   };
 
-  # Tracking nixos-20.09 as of 2021-05-25.
+  # Tracking nixos-21.05 as of 2021-06-02.
   stableHashes = {
-    commit = "ac60476ed94fd5424d9f3410c438825f793a8cbb";
-    sha256 = "1dlvpdsy5v09c7rj5f7xgakyj722yqr4415davjpcmrk4n5kw76v";
+    commit = "eaba7870ffc3400eca4407baa24184b7fe337ec1";
+    sha256 = "115disiz4b08iw46cidc7lm0advrxn5g2ldmlrxd53zf03skyb2w";
   };
 
   # import the nixos-unstable package set, or optionally use the
@@ -47,9 +47,7 @@ let
   # Overlay for packages that should come from the stable channel
   # instead (e.g. because something is broken in unstable).
   stableOverlay = self: super: {
-    inherit (stableNixpkgs)
-      awscli # TODO(grfn): Move back to unstable once it is fixed
-      ;
+    # nothing picked from stable currently
   };
 in import nixpkgsSrc {
   config.allowUnfree = true;
