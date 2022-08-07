bcb9f2497213592a28147413d55342ae708c8e4c
Vincent Ambo <mail@tazj.in>
2022-08-08 02:16:02 +0300

    feat(tvix/eval): add error variant for runtime type errors
    Change-Id: I74155cf01766b7a991a69522945bff67fbca5a16
    Reviewed-on: https://cl.tvl.fyi/c/depot/+/6073
    Tested-by: BuildkiteCI
    Reviewed-by: grfn <grfn@gws.fyi>

--- eval/src/errors.rs
+++ eval/src/errors.rs
@@ -1,7 +1,12 @@
 use std::fmt::Display;
 
 #[derive(Debug)]
-pub struct Error {}
+pub enum Error {
+    TypeError {
+        expected: &'static str,
+        actual: &'static str,
+    },
+}
 
 impl Display for Error {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
