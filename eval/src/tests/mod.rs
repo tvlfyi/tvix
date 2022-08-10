a949ac8056b0421f9074227b488ffd83eb2556b1
Vincent Ambo <mail@tazj.in>
2022-08-10 20:02:05 +0300

    test(tvix/eval): add some eval-okay-* tests for trivial types
    Change-Id: I85ccc07e08c67abf4fcd3752c58e1702943239ac
    Reviewed-on: https://cl.tvl.fyi/c/depot/+/6135
    Tested-by: BuildkiteCI
    Reviewed-by: sterni <sternenseemann@systemli.org>

--- eval/src/tests/mod.rs
+++ eval/src/tests/mod.rs
@@ -2,6 +2,25 @@ use crate::eval::interpret;
 
 use test_generator::test_resources;
 
+fn eval_okay_test(code_path: &str) {
+    let base = code_path
+        .strip_suffix("nix")
+        .expect("test files always end in .nix");
+    let exp_path = format!("{}exp", base);
+
+    let code = std::fs::read_to_string(code_path).expect("should be able to read test code");
+    let exp = std::fs::read_to_string(exp_path).expect("should be able to read test expectation");
+
+    let result = interpret(&code).expect("evaluation of eval-okay test should succeed");
+    let result_str = format!("{}", result);
+
+    assert_eq!(
+        exp.trim(),
+        result_str,
+        "result value representation (right) must match expectation (left)"
+    );
+}
+
 // identity-* tests contain Nix code snippets which should evaluate to
 // themselves exactly (i.e. literals).
 #[test_resources("src/tests/tvix_tests/identity-*.nix")]
@@ -23,25 +42,14 @@ fn identity(code_path: &str) {
 //
 // These evaluations are always supposed to succeed, i.e. all snippets
 // are guaranteed to be valid Nix code.
+#[test_resources("src/tests/tvix_tests/eval-okay-*.nix")]
+fn eval_okay(code_path: &str) {
+    eval_okay_test(code_path)
+}
 
 // eval-okay-* tests from the original Nix test suite.
 #[cfg(feature = "nix_tests")]
 #[test_resources("src/tests/nix_tests/eval-okay-*.nix")]
 fn nix_eval_okay(code_path: &str) {
-    let base = code_path
-        .strip_suffix("nix")
-        .expect("test files always end in .nix");
-    let exp_path = format!("{}exp", base);
-
-    let code = std::fs::read_to_string(code_path).expect("should be able to read test code");
-    let exp = std::fs::read_to_string(exp_path).expect("should be able to read test expectation");
-
-    let result = interpret(&code).expect("evaluation of eval-okay test should succeed");
-    let result_str = format!("{}", result);
-
-    assert_eq!(
-        exp.trim(),
-        result_str,
-        "result value representation (right) must match expectation (left)"
-    );
+    eval_okay_test(code_path)
 }
