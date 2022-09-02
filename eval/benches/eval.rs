fbb8b50dd68ff28a2ca3bc19fdc86600b33ef28b
Griffin Smith <root@gws.fyi>
2022-09-02 14:51:56 -0400

    fix(tvix/eval): Fix build of benchmarks
    Interpret was updated to take an optional path arg in
    6fe5e2d75 (feat(tvix/eval): resolve relative path literals, 2022-08-12),
    but since benchmarks aren't building in CI the resulting breakage of
    benchmarks was missed.
    
    Change-Id: I8a93f1b25ae62e2d032fafc153d91977c6466712
    Reviewed-on: https://cl.tvl.fyi/c/depot/+/6284
    Reviewed-by: tazjin <tazjin@tvl.su>
    Tested-by: BuildkiteCI

--- eval/benches/eval.rs
+++ eval/benches/eval.rs
@@ -2,7 +2,7 @@ use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use tvix_eval::interpret;
 
 fn eval_literals(c: &mut Criterion) {
-    c.bench_function("int", |b| b.iter(|| black_box(interpret("42"))));
+    c.bench_function("int", |b| b.iter(|| black_box(interpret("42", None))));
 }
 
 criterion_group!(benches, eval_literals);
