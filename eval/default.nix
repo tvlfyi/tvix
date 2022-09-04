fa8d92bb3218df44e45bbb84d137ce6c823a4879
sterni <sternenseemann@systemli.org>
2022-09-04 19:09:30 +0200

    fix(tvix/eval): don't rebuild drv upon changes in cargo target dir
    This avoids unnecessary rebuilds of //tvix/eval when working on it
    locally using impure cargo.
    
    Change-Id: I028033a6345a9655e2877a534448706b8f85a1a1
    Reviewed-on: https://cl.tvl.fyi/c/depot/+/6317
    Tested-by: BuildkiteCI
    Autosubmit: sterni <sternenseemann@systemli.org>
    Reviewed-by: tazjin <tazjin@tvl.su>

--- eval/default.nix
+++ eval/default.nix
@@ -2,7 +2,10 @@
 
 lib.fix (self:
 depot.third_party.naersk.buildPackage {
-  src = ./.;
+  src = depot.third_party.gitignoreSource ./.;
+  # see https://github.com/nix-community/naersk/issues/169
+  root = depot.nix.sparseTree ./. [ ./Cargo.lock ./Cargo.toml ];
+
   doCheck = true;
   cargoBuildOptions = opts: opts ++ [ "--all-targets" ];
 
