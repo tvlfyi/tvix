7b07354a50bd1f960a6d4d4efa90cfa20297c0cf
William Carroll <wpcarro@gmail.com>
2022-09-05 22:17:04 -0700

    feat(tvix/eval): Support builtins.bitOr
    Bitwise `or` on integers.
    
    Change-Id: I45b0897331d1a9b6840f9d0feedcf10acc67fcec
    Reviewed-on: https://cl.tvl.fyi/c/depot/+/6549
    Autosubmit: wpcarro <wpcarro@gmail.com>
    Reviewed-by: wpcarro <wpcarro@gmail.com>
    Reviewed-by: tazjin <tazjin@tvl.su>
    Tested-by: BuildkiteCI

---
+++ eval/src/tests/tvix_tests/eval-okay-builtins-bitor.nix
@@ -0,0 +1,10 @@
+[
+  (builtins.bitOr 0 0)
+  (builtins.bitOr 1 0)
+  (builtins.bitOr 0 1)
+  (builtins.bitOr 1 1)
+  (builtins.bitOr 8 8)
+  (builtins.bitOr 8 (builtins.add 4 4))
+  (builtins.bitOr (builtins.add 4 4) 8)
+  (builtins.bitOr (builtins.add 4 4) (builtins.add 4 4))
+]
