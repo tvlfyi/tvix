847a0649f605e4970ed8194850b96728b68ee73c
William Carroll <wpcarro@gmail.com>
2022-09-05 22:18:43 -0700

    feat(tvix/eval): Support builtins.bitXor
    Bitwise "exclusive-or" on integers.
    
    Change-Id: I90a0a15afb3a58662d70e82ea14e48b877476e04
    Reviewed-on: https://cl.tvl.fyi/c/depot/+/6550
    Autosubmit: wpcarro <wpcarro@gmail.com>
    Reviewed-by: wpcarro <wpcarro@gmail.com>
    Reviewed-by: tazjin <tazjin@tvl.su>
    Tested-by: BuildkiteCI

---
+++ eval/src/tests/tvix_tests/eval-okay-builtins-bitxor.nix
@@ -0,0 +1,12 @@
+[
+  (builtins.bitXor 0 0)
+  (builtins.bitXor 1 0)
+  (builtins.bitXor 0 1)
+  (builtins.bitXor 1 1)
+  (builtins.bitXor 8 0)
+  (builtins.bitXor 0 8)
+  (builtins.bitXor 8 8)
+  (builtins.bitXor 8 (builtins.add 4 4))
+  (builtins.bitXor (builtins.add 4 4) 8)
+  (builtins.bitXor (builtins.add 4 4) (builtins.add 4 4))
+]
