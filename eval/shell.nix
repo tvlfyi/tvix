b02350409ede128613a8b0ddf1d0cb50913a0ea8
sterni <sternenseemann@systemli.org>
2022-09-17 13:45:11 +0200

    chore(tvix/eval): add simple .envrc / shell.nix
    Change-Id: I10a44b92391a8ac4d7ec5e4b878953c215f59908
    Reviewed-on: https://cl.tvl.fyi/c/depot/+/6620
    Tested-by: BuildkiteCI
    Reviewed-by: tazjin <tazjin@tvl.su>

---
+++ eval/shell.nix
@@ -0,0 +1,12 @@
+{ depot ? import ../../. { }
+, pkgs ? depot.third_party.nixpkgs
+}:
+
+pkgs.mkShell {
+  name = "tvix-eval-dev-env";
+  packages = [
+    pkgs.cargo
+    pkgs.rustc
+    pkgs.clippy
+  ];
+}
