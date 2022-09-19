d0dde5eb17bd8a3b6e046e6f71e2d6f23c36ddcc
sterni <sternenseemann@systemli.org>
2022-09-19 12:52:24 +0200

    test(tvix/eval): assert flipped compareVersions behaves correctly
    Change-Id: I7c239340de5c83a3124dc6a8ba0c70290466966d
    Reviewed-on: https://cl.tvl.fyi/c/depot/+/6698
    Reviewed-by: grfn <grfn@gws.fyi>
    Reviewed-by: tazjin <tazjin@tvl.su>
    Tested-by: BuildkiteCI

--- eval/src/tests/tvix_tests/eval-okay-builtins-compareVersions.nix
+++ eval/src/tests/tvix_tests/eval-okay-builtins-compareVersions.nix
@@ -1,12 +1,21 @@
+let
+  cmp = a: b:
+    let
+      ord1 = builtins.compareVersions a b;
+      ord2 = builtins.compareVersions b a;
+    in
+      assert ord1 == -ord2; ord1;
+in
+
 [
-  (builtins.compareVersions "1.2.3" "1.2.3")
-  (builtins.compareVersions "1.2.2" "1.2.3")
-  (builtins.compareVersions "1.2.3" "1.2.40")
-  (builtins.compareVersions "1.2.3" ".1.2.3")
-  (builtins.compareVersions "1.2.3" "1..2.3")
-  (builtins.compareVersions "1.2.3" "1.2.3.")
-  (builtins.compareVersions "1.2.3" "1.2")
-  (builtins.compareVersions "1.2.3" "1.2.a")
-  (builtins.compareVersions "1a.b" "1a.2")
-  (builtins.compareVersions "1" "")
+  (cmp "1.2.3" "1.2.3")
+  (cmp "1.2.2" "1.2.3")
+  (cmp "1.2.3" "1.2.40")
+  (cmp "1.2.3" ".1.2.3")
+  (cmp "1.2.3" "1..2.3")
+  (cmp "1.2.3" "1.2.3.")
+  (cmp "1.2.3" "1.2")
+  (cmp "1.2.3" "1.2.a")
+  (cmp "1a.b" "1a.2")
+  (cmp "1" "")
 ]
