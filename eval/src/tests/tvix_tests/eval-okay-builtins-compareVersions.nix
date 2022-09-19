3c42e08073be862c7b071679496051535eda7d8f
sterni <sternenseemann@systemli.org>
2022-09-19 13:28:01 +0200

    test(tvix/eval): add more compareVersions cases from C++ Nix
    Change-Id: I25e7e7a2c547d0874e1e949bf96e6e066b1075ed
    Reviewed-on: https://cl.tvl.fyi/c/depot/+/6705
    Tested-by: BuildkiteCI
    Reviewed-by: tazjin <tazjin@tvl.su>

--- eval/src/tests/tvix_tests/eval-okay-builtins-compareVersions.nix
+++ eval/src/tests/tvix_tests/eval-okay-builtins-compareVersions.nix
@@ -18,4 +18,18 @@ in
   (cmp "1.2.3" "1.2.a")
   (cmp "1a.b" "1a.2")
   (cmp "1" "")
+  (cmp "1.0" "1.0.0")
+  # Subset of test cases from eval-okay-versions.nix shipped by C++ Nix
+  (cmp "1.0" "2.3")
+  (cmp "2.1" "2.3")
+  (cmp "2.3" "2.3")
+  (cmp "2.5" "2.3")
+  (cmp "3.1" "2.3")
+  (cmp "2.3.1" "2.3")
+  (cmp "2.3.1" "2.3a")
+  (cmp "2.3pre1" "2.3")
+  (cmp "2.3pre3" "2.3pre12")
+  (cmp "2.3a" "2.3c")
+  (cmp "2.3pre1" "2.3c")
+  (cmp "2.3pre1" "2.3q")
 ]
