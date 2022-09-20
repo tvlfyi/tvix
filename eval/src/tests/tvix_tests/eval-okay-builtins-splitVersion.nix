7f5cbe2474ed302d7c6b1a2ad032dcccafd6fcfb
sterni <sternenseemann@systemli.org>
2022-09-20 12:52:57 +0200

    test(tvix/eval): add test cases for builtins.splitVersion
    Change-Id: I60648f6c5cea34d19afb5ced3e98051201190a78
    Reviewed-on: https://cl.tvl.fyi/c/depot/+/6711
    Reviewed-by: grfn <grfn@gws.fyi>
    Tested-by: BuildkiteCI

---
+++ eval/src/tests/tvix_tests/eval-okay-builtins-splitVersion.nix
@@ -0,0 +1,13 @@
+[
+  (builtins.splitVersion "1.2.3")
+  (builtins.splitVersion "2.3.16")
+  (builtins.splitVersion "22.11pre408963.823e2c9b0a0")
+  (builtins.splitVersion "9.4.1-rc1")
+  (builtins.splitVersion "9.4.0.20220721")
+  (builtins.splitVersion "0.1-alpha")
+  (builtins.splitVersion "unstable-2022-09-20")
+  (builtins.splitVersion "30.pre9")
+  (builtins.splitVersion "0.pre+date=2021-11-30")
+  (builtins.splitVersion "1.2.0_pre23")
+  (builtins.splitVersion "0.1.0pre71_170f840")
+]
