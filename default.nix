d5eccc4b11141afee2e9519f6a21cc696f9bdee3
Vincent Ambo <mail@tazj.in>
2022-12-08 16:55:47 +0300

    refactor(tvix): provide a combined //tvix:shell mkShell target
    Manually maintained shell target based on the previous "loose"
    `shell.nix` file.
    
    We might want to have a function that combines the dependencies of all
    the targets automatically, but at a quick glance that was actually
    non-trivial so I'm leaving it as an exercise for someone else.
    
    Change-Id: I74754940088f1b58e3b6754fb782470c80ea4292
    Reviewed-on: https://cl.tvl.fyi/c/depot/+/7540
    Autosubmit: tazjin <tazjin@tvl.su>
    Reviewed-by: sterni <sternenseemann@systemli.org>
    Tested-by: BuildkiteCI

--- default.nix
+++ default.nix
@@ -7,4 +7,24 @@
     inherit pkgs;
     nixpkgs = pkgs.path;
   };
+
+  # Provide a shell for the combined dependencies of all Tvix Rust
+  # projects. Note that as this is manually maintained it may be
+  # lacking something, but it is required for some people's workflows.
+  #
+  # This shell can be entered with e.g. `mg shell //tvix:shell`.
+  shell = pkgs.mkShell {
+    name = "tvix-rust-dev-env";
+    packages = [
+      pkgs.buf-language-server
+      pkgs.cargo
+      pkgs.clippy
+      pkgs.rust-analyzer
+      pkgs.rustc
+      pkgs.rustfmt
+      pkgs.protobuf
+    ];
+  };
+
+  meta.ci.targets = [ "shell" ];
 }
