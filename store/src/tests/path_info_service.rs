24b269d65ff248816fb1e17ac0748701dd6f5bc5
Florian Klink <flokli@flokli.de>
2023-01-18 20:37:48 +0100

    refactor(tvix/store): simplify test a bit
    Import more things, and use expect_err to unpack the response.
    
    Change-Id: Ia319dd4d126b8d0e1df585234710d825a33a0002
    Reviewed-on: https://cl.tvl.fyi/c/depot/+/7868
    Tested-by: BuildkiteCI
    Reviewed-by: tazjin <tazjin@tvl.su>

--- store/src/tests/path_info_service.rs
+++ store/src/tests/path_info_service.rs
@@ -1,8 +1,11 @@
 use tempfile::TempDir;
+use tonic::Request;
 
+use crate::proto::get_path_info_request::ByWhat::ByOutputHash;
+use crate::proto::node::Node::Symlink;
 use crate::proto::path_info_service_server::PathInfoService;
-use crate::proto::GetPathInfoRequest;
-use crate::proto::{get_path_info_request, PathInfo};
+use crate::proto::PathInfo;
+use crate::proto::{GetPathInfoRequest, Node, SymlinkNode};
 use crate::sled_path_info_service::SledPathInfoService;
 
 use lazy_static::lazy_static;
@@ -20,19 +23,13 @@ async fn not_found() -> anyhow::Result<()> {
     let service = SledPathInfoService::new(TempDir::new()?.path().to_path_buf())?;
 
     let resp = service
-        .get(tonic::Request::new(GetPathInfoRequest {
-            by_what: Some(get_path_info_request::ByWhat::ByOutputHash(
-                DUMMY_OUTPUT_HASH.to_vec(),
-            )),
+        .get(Request::new(GetPathInfoRequest {
+            by_what: Some(ByOutputHash(DUMMY_OUTPUT_HASH.to_vec())),
         }))
         .await;
 
-    match resp {
-        Err(status) => {
-            assert_eq!(status.code(), tonic::Code::NotFound);
-        }
-        Ok(_) => panic!("must fail"),
-    };
+    let resp = resp.expect_err("must fail");
+    assert_eq!(resp.code(), tonic::Code::NotFound);
 
     Ok(())
 }
@@ -43,27 +40,23 @@ async fn put_get() -> anyhow::Result<()> {
     let service = SledPathInfoService::new(TempDir::new()?.path().to_path_buf())?;
 
     let path_info = PathInfo {
-        node: Some(crate::proto::Node {
-            node: Some(crate::proto::node::Node::Symlink(
-                crate::proto::SymlinkNode {
-                    name: "00000000000000000000000000000000-foo".to_string(),
-                    target: "doesntmatter".to_string(),
-                },
-            )),
+        node: Some(Node {
+            node: Some(Symlink(SymlinkNode {
+                name: "00000000000000000000000000000000-foo".to_string(),
+                target: "doesntmatter".to_string(),
+            })),
         }),
         ..Default::default()
     };
 
-    let resp = service.put(tonic::Request::new(path_info.clone())).await;
+    let resp = service.put(Request::new(path_info.clone())).await;
 
     assert!(resp.is_ok());
     assert_eq!(resp.expect("must succeed").into_inner(), path_info);
 
     let resp = service
-        .get(tonic::Request::new(GetPathInfoRequest {
-            by_what: Some(get_path_info_request::ByWhat::ByOutputHash(
-                DUMMY_OUTPUT_HASH.to_vec(),
-            )),
+        .get(Request::new(GetPathInfoRequest {
+            by_what: Some(ByOutputHash(DUMMY_OUTPUT_HASH.to_vec())),
         }))
         .await;
 
