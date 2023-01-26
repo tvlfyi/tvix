0a14cc3c75dd81a17f763fd63edea9f1da28bb84
Florian Klink <flokli@flokli.de>
2023-01-26 16:54:07 +0100

    refactor(tvix/derivation): align error messages with rust style
    Change-Id: I9ccd4c043bdddefee98a2c0b3d6eb7d9cb53c454
    Reviewed-on: https://cl.tvl.fyi/c/depot/+/7935
    Reviewed-by: tazjin <tazjin@tvl.su>
    Tested-by: BuildkiteCI
    Autosubmit: flokli <flokli@flokli.de>

--- derivation/src/errors.rs
+++ derivation/src/errors.rs
@@ -5,40 +5,40 @@ use tvix_store::store_path::ParseStorePathError;
 #[derive(Debug, Error)]
 pub enum DerivationError {
     // outputs
-    #[error("No outputs defined.")]
+    #[error("no outputs defined")]
     NoOutputs(),
-    #[error("Invalid output name: {0}.")]
+    #[error("invalid output name: {0}")]
     InvalidOutputName(String),
-    #[error("Encountered fixed-output derivation, but more than 1 output in total.")]
+    #[error("encountered fixed-output derivation, but more than 1 output in total")]
     MoreThanOneOutputButFixed(),
-    #[error("Invalid output name for fixed-output derivation: {0}.")]
+    #[error("invalid output name for fixed-output derivation: {0}")]
     InvalidOutputNameForFixed(String),
-    #[error("Unable to validate output {0}: {1}.")]
+    #[error("unable to validate output {0}: {1}")]
     InvalidOutput(String, OutputError),
     // input derivation
-    #[error("Unable to parse input derivation path {0}: {1}.")]
+    #[error("unable to parse input derivation path {0}: {1}")]
     InvalidInputDerivationPath(String, ParseStorePathError),
-    #[error("Input Derivation {0} doesn't end with .drv.")]
+    #[error("input derivation {0} doesn't end with .drv")]
     InvalidInputDerivationPrefix(String),
-    #[error("Input Derivation {0} output names are empty.")]
+    #[error("input derivation {0} output names are empty")]
     EmptyInputDerivationOutputNames(String),
-    #[error("Input Derivation {0} output name {1} is invalid.")]
+    #[error("input derivation {0} output name {1} is invalid")]
     InvalidInputDerivationOutputName(String, String),
 
     // input sources
-    #[error("Unable to parse input sources path {0}: {1}.")]
+    #[error("unable to parse input sources path {0}: {1}")]
     InvalidInputSourcesPath(String, ParseStorePathError),
 
     // platform
-    #[error("Invalid platform field: {0}")]
+    #[error("invalid platform field: {0}")]
     InvalidPlatform(String),
 
     // builder
-    #[error("Invalid builder field: {0}")]
+    #[error("invalid builder field: {0}")]
     InvalidBuilder(String),
 
     // environment
-    #[error("Invalid environment key {0}")]
+    #[error("invalid environment key {0}")]
     InvalidEnvironmentKey(String),
 }
 
