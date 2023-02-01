7f439cb1c507383ac0913fea2810f01b168ea996
Florian Klink <flokli@flokli.de>
2023-02-01 14:27:41 +0100

    feat(views/tvix): add tvix josh workspace
    This provides a trimmed-down version of //tvix, that allows building
    tvix without all of the monorepo cloned.
    
    We need to manually vendor in our patch for evans and ``--bytes-as-
    base64`, as we don't have our depot overlays available in the josh
    workspace.
    
    Fixes https://b.tvl.fyi/issues/247.
    
    Change-Id: I24306b8bb16ebf0df238b8f1eee0d47655a14827
    Reviewed-on: https://cl.tvl.fyi/c/depot/+/8000
    Autosubmit: flokli <flokli@flokli.de>
    Reviewed-by: tazjin <tazjin@tvl.su>
    Tested-by: BuildkiteCI

--- default.nix
+++ default.nix
@@ -1,62 +1,36 @@
-# Nix helpers for projects under //tvix
-{ pkgs, depot, ... }:
+# Externally importable TVL depot stack. This is intended to be called
+# with a supplied package set, otherwise the package set currently in
+# use by the TVL depot will be used.
+#
+{ pkgs ? (import ./nixpkgs {
+    depotOverlays = false;
+    depot.third_party.sources = import ./sources { };
+  })
+, ...
+}:
 
 let
-  # crate override for crates that need protobuf
-  protobufDep = prev: (prev.nativeBuildInputs or [ ]) ++ [ pkgs.protobuf ];
-in
-{
-  # Load the crate2nix crate tree.
-  crates = import ./Cargo.nix {
-    inherit pkgs;
-    nixpkgs = pkgs.path;
-
-    defaultCrateOverrides = pkgs.defaultCrateOverrides // {
-      prost-build = prev: {
-        nativeBuildInputs = protobufDep prev;
-      };
-
-      tonic-reflection = prev: {
-        nativeBuildInputs = protobufDep prev;
-      };
-
-      tvix-store = prev: {
-        PROTO_ROOT = depot.tvix.store.protos;
-        nativeBuildInputs = protobufDep prev;
-      };
-
-      tvix-store-bin = prev: {
-        PROTO_ROOT = depot.tvix.store.protos;
-        nativeBuildInputs = protobufDep prev;
-      };
-    };
-  };
-
-  # Run crate2nix generate in the current working directory, then
-  # format the generated file with depotfmt.
-  crate2nixGenerate = pkgs.writeShellScriptBin "crate2nix-generate" ''
-    ${pkgs.crate2nix}/bin/crate2nix generate
-    ${depot.tools.depotfmt}/bin/depotfmt Cargo.nix
-  '';
-
-  # Provide a shell for the combined dependencies of all Tvix Rust
-  # projects. Note that as this is manually maintained it may be
-  # lacking something, but it is required for some people's workflows.
-  #
-  # This shell can be entered with e.g. `mg shell //tvix:shell`.
-  shell = pkgs.mkShell {
-    name = "tvix-rust-dev-env";
-    packages = [
-      pkgs.buf-language-server
-      pkgs.cargo
-      pkgs.clippy
-      pkgs.evans
-      pkgs.rust-analyzer
-      pkgs.rustc
-      pkgs.rustfmt
-      pkgs.protobuf
+  # `Call $methodName --bytes-as-base64` support for evans
+  evans = pkgs.evans.overrideAttrs (old: {
+    patches = old.patches or [ ] ++ [
+      (pkgs.fetchpatch {
+        url = "https://github.com/ktr0731/evans/pull/611/commits/f2109627c0d20588980fe6fd6348d223dbdf7c33.patch";
+        hash = "sha256-ff8drvAYwQvHeymaHEruvwDYynClpzPM5lrB7IeQHBs=";
+      })
     ];
-  };
+  });
+in
+pkgs.mkShell {
+  name = "tvix-rust-dev-env";
+  packages = [
+    pkgs.buf-language-server
+    pkgs.cargo
+    pkgs.clippy
+    pkgs.rust-analyzer
+    pkgs.rustc
+    pkgs.rustfmt
+    pkgs.protobuf
 
-  meta.ci.targets = [ "shell" ];
+    evans
+  ];
 }
