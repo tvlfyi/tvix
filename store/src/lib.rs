66ff6d46775d76a8f31b5a460d576b509eefe461
Florian Klink <flokli@flokli.de>
2023-02-12 12:08:10 +0100

    feat(tvix/store): add blobreader
    A BlobReader can be used to read a blob, which might consist out of
    multiple chunks. Chunks are fetched from a ChunkService.
    
    Change-Id: I1806225b0052adaa4a2320b79b744e554e524ee3
    Reviewed-on: https://cl.tvl.fyi/c/depot/+/8088
    Tested-by: BuildkiteCI
    Reviewed-by: raitobezarius <tvl@lahfa.xyz>
    Reviewed-by: tazjin <tazjin@tvl.su>

--- store/src/lib.rs
+++ store/src/lib.rs
@@ -1,11 +1,13 @@
 pub mod client;
 
+mod blobreader;
 mod errors;
 
 pub mod blobservice;
 pub mod chunkservice;
 pub mod proto;
 
+pub use blobreader::BlobReader;
 pub mod dummy_blob_service;
 pub mod sled_directory_service;
 pub mod sled_path_info_service;
