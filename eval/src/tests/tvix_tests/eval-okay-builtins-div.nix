f1e859399d5d56e71a661c7939ec1812e049bb04
sterni <sternenseemann@systemli.org>
2023-08-05 17:58:23 +0200

    test(tvix/eval): check truncation direction of builtins.div
    builtins.div ought to truncate towards zero so that
    
        -(builtins.div a b) == builtins.div (-a) b
        -(builtins.div a b) == builtins.div a (-b)
    
    Change-Id: I8b7c08cd7f4fa8a1363c786d42c8d484f6cd133d
    Reviewed-on: https://cl.tvl.fyi/c/depot/+/9006
    Autosubmit: sterni <sternenseemann@systemli.org>
    Reviewed-by: tazjin <tazjin@tvl.su>
    Tested-by: BuildkiteCI

--- eval/src/tests/tvix_tests/eval-okay-builtins-div.nix
+++ eval/src/tests/tvix_tests/eval-okay-builtins-div.nix
@@ -8,4 +8,27 @@
   (builtins.div 1 2.0)
   (builtins.div 1.0 2.0)
   (builtins.div (builtins.div 84 4) 0.5)
+
+  # builtins.div should truncate towards 0
+  (builtins.div 3 2)
+  (builtins.div (-3) (-2))
+  (builtins.div (-3) 2)
+  (builtins.div 3 (-2))
+  (-(builtins.div 3 2))
+
+  (builtins.div 4 3)
+  (builtins.div (-4) (-3))
+  (builtins.div (-4) 3)
+  (builtins.div 4 (-3))
+  (-(builtins.div 4 3))
+
+  (builtins.div 5 3)
+  (builtins.div (-5) (-3))
+  (builtins.div (-5) 3)
+  (builtins.div 5 (-3))
+  (-(builtins.div 5 3))
+
+  (builtins.div 2147812578 (-28748))
+  (builtins.div (-2147812578) 28748)
+  (-(builtins.div 2147812578 28748))
 ]
