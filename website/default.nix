4c605b4b6110c72798ff5a3e7922e413adb1b892
Florian Klink <flokli@flokli.de>
2023-09-07 17:49:41 +0300

    refactor(tvix/website): remove unused structuredData
    Change-Id: I2445b977c5bed3e2d4b90e8ad81119d316b29b50
    Reviewed-on: https://cl.tvl.fyi/c/depot/+/9281
    Reviewed-by: tazjin <tazjin@tvl.su>
    Reviewed-by: Adam Joseph <adam@westernsemico.com>
    Tested-by: BuildkiteCI
    Autosubmit: flokli <flokli@flokli.de>

--- website/default.nix
+++ website/default.nix
@@ -1,14 +1,6 @@
 { depot, lib, pkgs, ... }:
 
 let
-  # https://developers.google.com/search/docs/advanced/structured-data/logo
-  structuredData = {
-    "@context" = "https://schema.org";
-    "@type" = "Organisation";
-    url = "https://tvl.fyi";
-    logo = "https://static.tvl.fyi/latest/logo-animated.svg";
-  };
-
   # All Tvix-related blog posts from the main TVL website
   tvixPosts = builtins.filter
     (post: !(post.draft or false) && (lib.hasInfix "Tvix" post.title))
