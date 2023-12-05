3e7cff2569352ad5131c3c49c02c11078bf11c0f
Aspen Smith <root@gws.fyi>
2023-12-05 11:58:20 -0500

    feat(tvix): Add script for running benchmarks in Windtunnel
    Currently this just uses a Docker container, which is gross but works
    fine for now since we don't have the ability to build benchmarks in nix
    as of cl/7538
    
    Change-Id: I48e317f44bc2c73533d7663403786a3a37c7952f
    Reviewed-on: https://cl.tvl.fyi/c/depot/+/10189
    Reviewed-by: flokli <flokli@flokli.de>
    Tested-by: BuildkiteCI
    Reviewed-by: grfn <grfn@gws.fyi>
    Autosubmit: grfn <grfn@gws.fyi>

---
+++ scripts/bench.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Benchmark script that runs inside the Windtunnel build agent
+
+set -euo pipefail
+
+echo "Running benchmarks for tvix/eval..."
+cd tvix/eval
+docker run --rm -v "$(pwd):/app" -w /app rust cargo bench
+windtunnel-cli report -f criterion-rust .
