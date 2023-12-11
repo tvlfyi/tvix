9e35be36e585e78b81f0c261b479d6462eb56512
Aspen Smith <grfn@gws.fyi>
2023-12-10 22:51:43 -0500

    feat(tvix): Benchmark nixpkgs eval with hyperfine
    Add some hyperfine benchmarks to Tvix's windtunnel benchmark script for
    evaluating the outPath of hello and a cross-compiled hello.
    
    Change-Id: I9d76e5ce0a3fd7d9c125c36c5fced675b660a8a8
    Reviewed-on: https://cl.tvl.fyi/c/depot/+/10248
    Reviewed-by: grfn <grfn@gws.fyi>
    Tested-by: BuildkiteCI
    Autosubmit: grfn <grfn@gws.fyi>

--- scripts/bench.sh
+++ scripts/bench.sh
@@ -1,4 +1,5 @@
-#!/usr/bin/env bash
+#!/usr/bin/env nix-shell
+#!nix-shell -i bash ../.. -A tvix.shell
 
 # Benchmark script that runs inside the Windtunnel build agent
 
@@ -6,5 +7,12 @@ set -euo pipefail
 
 echo "Running benchmarks for tvix/eval..."
 cd "$(dirname "$(dirname "$0")")/eval"
-docker run --rm -v "$(pwd):/app" -w /app rust cargo bench
+cargo bench
 windtunnel-cli report -f criterion-rust .
+
+echo "Running tvix macrobenchmarks..."
+cargo build --release --bin tvix
+hyperfine --export-json ./results.json \
+    -n 'eval-nixpkgs-hello' "target/release/tvix -E '(import ../../nixpkgs {}).hello.outPath'" \
+    -n 'eval-nixpkgs-cross-hello' "target/release/tvix -E '(import ../../nixpkgs {}).pkgsCross.aarch64-multiplatform.hello.outPath'"
+windtunnel-cli report -f hyperfine-json ./results.json
