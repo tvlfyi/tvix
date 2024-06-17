a3e01c57df53dc17abf89e82ea2f439b7a3d86c4
Ilan Joselevich <personal@ilanjoselevich.com>
2024-06-17 18:14:23 +0300

    chore(tvix): patch crate2nix to drop darwin dontStrip
    This patch has already been applied on upstream but there hasn't been an
    official release yet so we patch it ourselves.
    
    Upstreamed patch: https://github.com/nix-community/crate2nix/commit/0209f258cda8a9972a785e26d92fb477ce4d1b0e
    
    Follow-up of: https://cl.tvl.fyi/c/depot/+/11856
    
    Change-Id: If56071ce5753fd26e2b4c203cd831bbe5d329009
    Reviewed-on: https://cl.tvl.fyi/c/depot/+/11858
    Reviewed-by: flokli <flokli@flokli.de>
    Tested-by: BuildkiteCI

--- Cargo.nix
+++ Cargo.nix
@@ -18000,8 +18000,6 @@ rec {
             buildRustCrateForPkgsFunc pkgs
               (
                 crateConfig // {
-                  # https://github.com/NixOS/nixpkgs/issues/218712
-                  dontStrip = stdenv.hostPlatform.isDarwin;
                   src = crateConfig.src or (
                     pkgs.fetchurl rec {
                       name = "${crateConfig.crateName}-${crateConfig.version}.tar.gz";
