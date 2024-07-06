33ae2f0cadfcfc9d93f352f36feb79f2afa3adc1
Aspen Smith <root@gws.fyi>
2024-07-06 11:04:21 -0400

    test(tvix/cli): Add some additional REPL tests
    A couple of already-passing tests covering REPL behavior
    
    Change-Id: Ie21f4abf68ab12827fd15128a8ef810cd8592d07
    Reviewed-on: https://cl.tvl.fyi/c/depot/+/11959
    Reviewed-by: flokli <flokli@flokli.de>
    Tested-by: BuildkiteCI
    Autosubmit: aspen <root@gws.fyi>

--- cli/tests/repl.rs
+++ cli/tests/repl.rs
@@ -25,3 +25,45 @@ test_repl!(simple_expr_eval() {
         => 1 :: int
     "#]];
 });
+
+test_repl!(multiline_input() {
+    "{ x = 1; " => expect![[""]];
+    "y = 2; }" => expect![[r#"
+        => { x = 1; y = 2; } :: set
+    "#]];
+});
+
+test_repl!(bind_literal() {
+    "x = 1" => expect![[""]];
+    "x" => expect![[r#"
+        => 1 :: int
+    "#]];
+});
+
+test_repl!(bind_lazy() {
+    "x = { z = 1; }" => expect![[""]];
+    "x" => expect![[r#"
+        => { z = 1; } :: set
+    "#]];
+    "x.z" => expect![[r#"
+        => 1 :: int
+    "#]];
+    "x.z" => expect![[r#"
+        => 1 :: int
+    "#]];
+});
+
+test_repl!(deep_print() {
+    "builtins.map (x: x + 1) [ 1 2 3 ]" => expect![[r#"
+        => [ <CODE> <CODE> <CODE> ] :: list
+    "#]];
+    ":p builtins.map (x: x + 1) [ 1 2 3 ]" => expect![[r#"
+        => [ 2 3 4 ] :: list
+    "#]];
+});
+
+test_repl!(explain() {
+    ":d { x = 1; y = [ 2 3 4 ]; }" => expect![[r#"
+        => a 2-item attribute set
+    "#]];
+});
